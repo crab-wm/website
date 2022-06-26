8b5f98b913a142572488b5b1901228f369547664
PiotrekPKP <peterpkppeter@gmail.com>
2022-06-26 11:42:30 +0200

    feat: Auto-fetching apps

--- components/content/AppsGrid.vue
+++ components/content/AppsGrid.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="apps-grid">
-    <slot />
+    <AppBlock v-for="app in apps" :image="app.image" :url="app._path">
+      <template v-slot:name>{{ app.name }}</template>
+      {{ app.info }}
+    </AppBlock>
   </div>
 </template>
+
+<script setup lang="ts">
+import ContentApp from "~/types/ContentApp";
+import { ParsedContent } from "@nuxt/content/dist/runtime/types";
+
+const apps = (await queryContent("apps").skip(1).find()) as (ParsedContent &
+  ContentApp)[];
+</script>
 
 <style lang="scss">
 @import "styles/mixins";
